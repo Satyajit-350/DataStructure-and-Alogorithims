9915b4c04536e95ef11fa0094579f5f60f2d176f
Satyajit Biswal <86532832+Satyajit-350@users.noreply.github.com>
2023-10-30 16:07:39 +0530

    Subset sum equals to k dp

---
+++ dynamic_programming/subset_sum_equal_k.java
@@ -0,0 +1,95 @@
+package dynamic_programming;
+
+import java.util.*;
+
+public class subset_sum_equal_k {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int k = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Answer is: " + subsetSumToK(n, k, arr));
+    }
+    public static boolean subsetSumToK(int n, int k, int arr[]){
+        // Write your code here.
+        int dp[][] = new int[n+1][k+1];
+        boolean dp1[][] = new boolean[n][k+1];
+        for(int a[]: dp){
+            Arrays.fill(a,-1);
+        }
+        // return solve(n-1, k, arr);
+        // return solveUsingMemoization(n-1,k,arr,dp);
+        return solveUsingTabulation(n, k, arr, dp1);
+    }
+    //Recursive
+    public static boolean solve(int idx, int target, int a[]){
+        //base case
+        if(target==0){
+            return true;
+        }
+        if(idx==0){
+            return a[0]==target;
+        }
+        //not pick case
+        boolean not_pick = solve(idx-1, target, a);
+        //pick case
+        boolean pick = false;
+        if(target>=a[idx]){
+            pick = solve(idx-1, target-a[idx], a);
+        }
+        return (pick || not_pick);
+    }
+    //Memoization
+    public static boolean solveUsingMemoization(int idx, int target, int a[], int dp[][]){
+        //base case
+        if(target==0){
+            return true;
+        }
+        if(idx==0){
+            return a[0]==target;
+        }
+        if(dp[idx][target]!=-1){
+            return (dp[idx][target]==0)?true:false;
+        }
+        //not pick case
+        boolean not_pick = solveUsingMemoization(idx-1, target, a, dp);
+        //pick case
+        boolean pick = false;
+        if(target>=a[idx]){
+            pick = solveUsingMemoization(idx-1, target-a[idx], a, dp);
+        }
+        boolean res = pick || not_pick;
+        if(res){
+            dp[idx][target] = 0;
+        }else{
+            dp[idx][target] = 1;
+        }
+        return res;
+    }
+    //Tabulation
+    public static boolean solveUsingTabulation(int n, int k, int a[], boolean dp[][]){
+        //base case
+        for(int i=0; i<n; i++){
+            dp[i][0] = true;
+        }
+        if(a[0]<=k){
+            dp[0][a[0]] = true;
+        }
+        for(int i=1; i<n; i++){
+            for(int target=1; target<=k; target++){
+                //not pick case
+                boolean not_take = dp[i-1][target];
+                //pick case
+                boolean take = false;
+                if(target>=a[i]){
+                    take = dp[i-1][target-a[i]];
+                }
+                dp[i][target] = take||not_take;
+            }
+        }
+        return dp[n-1][k];
+    }
+}
