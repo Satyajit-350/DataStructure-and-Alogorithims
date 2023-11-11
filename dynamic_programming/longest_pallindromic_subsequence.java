2863070c4dc063caffa5e9a5fd964029addcf127
Satyajit Biswal <86532832+Satyajit-350@users.noreply.github.com>
2023-11-11 11:51:12 +0530

    Longest pallindromic substring

---
+++ dynamic_programming/longest_pallindromic_subsequence.java
@@ -0,0 +1,42 @@
+package dynamic_programming;
+
+import java.util.*;
+
+public class longest_pallindromic_subsequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println("Answer is: " + longestPalindromeSubsequence(s));
+    }
+    public static int longestPalindromeSubsequence(String s) {
+		// Write your code here.
+		int n = s.length();
+		StringBuilder sb = new StringBuilder(s);
+		String t = sb.reverse().toString(); 
+		int m = t.length();
+		int dp[][] = new int[n+1][m+1];
+        for(int a[]: dp){
+            Arrays.fill(a,-1);
+        }
+        return solveUsingTabulation(n, m, s, t, dp);
+	}
+	 //Tabulation
+    public static int solveUsingTabulation(int n, int m, String s, String t, int dp[][]){
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 0;
+        }
+        for(int j=0; j<=m; j++){
+            dp[0][j] = 0;
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 0 + Math.max(dp[i-1][j], dp[i][j-1]);
+                }	
+            }
+        }
+        return dp[n][m];
+    }
+}
